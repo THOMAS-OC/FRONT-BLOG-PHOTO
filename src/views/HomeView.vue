28d2ee53461a005bd7292686482f0f6891faac5a
= <=>
2023-11-08 09:41:35 +0100

    maj BDD

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -67,7 +67,7 @@ export default {
     filterJsonFile (album) {
     this.currentJsonFile = null
     this.currentJsonFile = this.saveJsonFile.filter(item => item.categorie == album)
-    this.currentJsonFile = this.currentJsonFile.reverse()
+    this.currentJsonFile = this.currentJsonFile.sort((a, b) => a.order - b.order)
     // Affichage de la section
     document.querySelector(".album").className = "album"
 
